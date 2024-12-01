023e35ca7eb928a81ef7c6b71c521fe8d9ab2fea
Sigmaboiii <aadikhatri45@gmail.com>
2024-12-01 23:42:16 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -8,20 +8,20 @@ $mail = new PHPMailer(true);
 
 try {
     // Server settings
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                   // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                 // Enable SMTP authentication
-    $mail->Username   = 'aadikhatri45@gmail.com';             // SMTP username
-    $mail->Password   = 'hjqe oqsb jbbf drwp';                // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;      // Enable TLS encryption
-    $mail->Port       = 587;                                  // TCP port to connect to
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.domain.com';                   // Replace it with the Gmail.
+    $mail->SMTPAuth   = true;                                
+    $mail->Username   = 'your_email@domain.com';             // Replace it with your Gmail account.
+    $mail->Password   = 'your_password';                // Replace it your Gmail account app password not your main account password (It is shown in ReadME how to create an app password.)
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;      
+    $mail->Port       = 587;                                  
 
     // Recipients
-    $mail->setFrom('aadikhatri45@gmail.com', 'Mailer');
-    $mail->addAddress('riturainakhatri@gmail.com', 'Joe User');   // Add a recipient
+    $mail->setFrom('your_email@domain.com', 'Mailer');       //Add your Gmail account.
+    $mail->addAddress('recipient@domain.com', 'Joe User');   // Add a recipient's Gmail account
 
     // Content
-    $mail->isHTML(true);                                      // Set email format to HTML
+    $mail->isHTML(true);                                 
     $mail->Subject = 'Motion Detected!';
     $mail->Body    = 'Motion has been detected by your camera.';
     $mail->AltBody = 'Motion has been detected by your camera.';
